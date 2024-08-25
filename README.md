4766e82216e98edb944050b3bdfbad1d5bc38f00
Matias Ezequiel Vara Larsen <matiasevara@torokernel.io>
2024-08-25 20:55:35 +0200

    Update README.md and add hashmap
    Signed-off-by: Matias Ezequiel Vara Larsen <matiasevara@torokernel.io>

---
+++ README.md
@@ -0,0 +1,2 @@
+# PObserver
+This crate allows you to programmatically set breakpoints to a program and execute functions when these breakpoints are hit. Breakpoints are just address. This create is based on ptrace. 
--- examples/example.rs
+++ examples/example.rs
@@ -1,21 +1,29 @@
 use pobserver::Observer;
+use std::collections::HashMap;
 
 fn main() {
     // TODO: Get addr from the name of the functions
-    let breakpoints = vec![0x55555555b8a0u64, 0x55555555b890u64];
+
+    let mut breakpoints = HashMap::new();
+
+    breakpoints.insert(0x55555555b8a0u64, "event1");
+    breakpoints.insert(0x55555555b890u64, "event2");
 
     let mut observer = Observer::new("../debugee/target/debug/debugee");
 
     // run child process and return
     observer.run();
 
-    // add breakpoints
-    for bp in breakpoints {
-        observer.add_breakpoint(bp);
+    // install breakpoints
+    for (addr, &_name) in breakpoints.iter() {
+        observer.add_breakpoint(*addr)
     }
 
     loop {
         let bp = observer.run_until_breakpoint();
-        println!("hit 0x{:x}", bp);
+        match breakpoints.get(&bp) {
+            Some(&name) => println!("Event {} happened", name),
+            _ => println!("Don't have Daniel's number."),
+        }
     }
 }
